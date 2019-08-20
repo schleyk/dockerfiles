2e6aa2bf6003e30306bbb767c8318082d382dd9e
Kevin Schley <info@it-schley.eu>
2019-08-20 18:43:04 +0200

    update Syspass to 3.1.0 change Guacamole file structure

---
+++ guacamole/build_requirements_dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/apache/guacamole-client.git
+mv guacamole-client guacamole-client-dev
+
--- guacamole/build_requirements_stable.sh
+++ guacamole/build_requirements_stable.sh
@@ -3,3 +3,5 @@
 git clone https://github.com/apache/guacamole-client.git
 cd guacamole-client/
 git checkout 1.0.0
+cd ..
+mv guacamole-client guacamole-client-stable
