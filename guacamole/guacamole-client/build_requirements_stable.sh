bb92b1c239481571a4e9473fdd436e0fb3cccff4
schleyk <info@it-schley.eu>
2023-07-09 21:22:41 +0200

    Software Update Update PHP to 8.1.21 / 8.2.8 Update Guacamole to 1.5.2

--- guacamole/guacamole-client/build_requirements_stable.sh
+++ guacamole/guacamole-client/build_requirements_stable.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/apache/guacamole-client.git
 cd guacamole-client/
-git checkout 1.5.1
+git checkout 1.5.2
 cd ..
 mv guacamole-client guacamole-client-stable
--- guacamole/guacamole-server/build_requirements_stable.sh
+++ guacamole/guacamole-server/build_requirements_stable.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/apache/guacamole-server.git
 cd guacamole-server/
-git checkout 1.5.1
+git checkout 1.5.2
 cd ..
 mv guacamole-server guacamole-server-stable
