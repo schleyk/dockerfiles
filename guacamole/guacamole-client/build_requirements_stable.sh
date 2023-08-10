8822378913a7fc6f8fc34cdbf86efc96f4f790d4
schleyk <info@it-schley.eu>
2023-08-10 23:47:44 +0200

    Update Guacamole and guacd to 1.5.3

--- guacamole/guacamole-client/build_requirements_stable.sh
+++ guacamole/guacamole-client/build_requirements_stable.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/apache/guacamole-client.git
 cd guacamole-client/
-git checkout 1.5.2
+git checkout 1.5.3
 cd ..
 mv guacamole-client guacamole-client-stable
--- guacamole/guacamole-server/build_requirements_stable.sh
+++ guacamole/guacamole-server/build_requirements_stable.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/apache/guacamole-server.git
 cd guacamole-server/
-git checkout 1.5.2
+git checkout 1.5.3
 cd ..
 mv guacamole-server guacamole-server-stable
