fd2b8c82500e077ee8e68203605553c566c86e94
schleyk <info@it-schley.eu>
2024-04-25 15:12:22 +0200

    fix build bug

--- guacamole/guacamole-server/build_requirements_stable.sh
+++ guacamole/guacamole-server/build_requirements_stable.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/apache/guacamole-client.git
-cd guacamole-client/
+git clone https://github.com/apache/guacamole-server.git
+cd guacamole-server/
 git checkout 1.5.5
 rm Dockerfile
 wget https://raw.githubusercontent.com/apache/guacamole-server/main/Dockerfile
 cd ..
-mv guacamole-client guacamole-client-stable
+mv guacamole-server guacamole-client-stable
